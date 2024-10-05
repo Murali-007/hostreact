86c28608f50fd53c84657c6fb55eb3e63a1cfaf9
Murali-007 <165800937+Murali-007@users.noreply.github.com>
2024-10-05 20:52:47 +0530

    Add files via upload

---
+++ Next-blog/my-app/src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Contact from "../components/Contact"
+
+export const metadata = {
+    title: "Contact Us Page",
+    description: "Murali Code contact form"
+}
+
+export default function Page() {
+   return <Contact/>
+}
