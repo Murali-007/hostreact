d9de0b105ca654542c9e9148e07b0d8f43e8664a
Murali-007 <165800937+Murali-007@users.noreply.github.com>
2024-10-05 21:01:49 +0530

    Add files via upload

---
+++ port/class1/src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Footer from './Components/Footer'
+import Headers from './Components/Headers'
+import Hero from './Components/Hero'
+import Projects from './Components/Projects'
+import Resume from './Components/Resume'
+
+
+function App() {
+
+      
+  return (
+   <div className='App'>
+
+
+   <Headers/>
+   <Hero/>
+   <About/>
+   <Projects/>
+   <Resume/>
+   <Contact/>
+   <Footer/>
+   
+   </div>
+   
+   
+   
+  )
+}
+
+export default App
---
+++ port/class1/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <div className="py-4 bg-secondary text-center text-white">&copy; JVLcode 2024</div>
+      
+    
+  )
+}
+
+export default Footer
